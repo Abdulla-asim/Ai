8cad0c8d0184551641118c41b562b7a502131a67
Abdullah <144978015+Abdulla-asim@users.noreply.github.com>
2024-09-14 10:09:35 +0000

    added some comments

--- lab1/Levenshtein.py
+++ lab1/Levenshtein.py
@@ -1,14 +1,18 @@
 def levenshtein(a, b):
 
+    # Get the lnghts of the strings
     m, n = len(a), len(b)
 
+    # Create a matrix of size (m+1 x n+1)
     matrix = [[0] * (n+1) for i in range(m+1)]
 
+    # Fill the first row and column of the matrix from 1 to m and n 
     for i in range(m+1):
       matrix[i][0] = i
     for j in range(n+1):
       matrix[0][j] = j
 
+    # Fill in the matrix (dynamic programming)
     for i in range(1, m+1):
       for j in range(1, n+1):
         insert = matrix[i-1][j] + 1
@@ -17,6 +21,7 @@ def levenshtein(a, b):
         # Choose minimum of the three
         matrix[i][j] = min(insert, delete, substitute)
 
+    # Print the matrix
     for i in range(m+1):
         for j in range(n+1):
             print(matrix[i][j], end = " ")
@@ -28,6 +33,6 @@ def levenshtein(a, b):
 a = input("Enter string 1: ")
 b = input("Enter string 2: ")
 
+# Calculate the edit distance and print it
 distance = levenshtein(a, b)
-
 print(f"\nEdit Distance: {distance}")
